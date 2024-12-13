c69930ec902ea54fa2ba06f3d1eda15b2a5cab0a
kunal23g <141353869+kunal23g@users.noreply.github.com>
2024-12-14 00:44:05 +0530

    Add files via upload

---
+++ src/components/FacialRecognition.jsx
@@ -0,0 +1,166 @@
+import { motion } from "framer-motion";
+import right from "../assets/right.jpg";
+import left from "../assets/left.jpg";
+import { ChartLine, ShoppingCart, PersonStanding, BookUp2 } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { FaRegHandPointRight } from "react-icons/fa";
+
+const Testimonials = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="py-10 max-w-[1240px] mx-auto">
+      <h1 className="text-5xl font-bold lg:text-7xl px-4 text-white mt-10">
+        FACIAL RECOGNITION
+      </h1>
+
+      <div
+        ref={ref}
+        className=" mt-10  p-2 max-w-[1240px] mx-auto my-3 md:grid grid-cols-2"
+      >
+        {/* Text Content */}
+        <div className="font-playfair col-span-1 p-3 lg:w-[80%] flex flex-col justify-center">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          ></motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 0.8 }}
+          >
+            <h1 className="text-4xl lg:text-5xl font-foundersGrotesk font-bold  text-dark-blue mb-4">
+              FEATURES
+            </h1>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 1.1 }}
+          >
+            <p className="text-xm lg:text-xl font-neueMontreal font-medium text-gray-600 mb-5">
+            Accurate Identity Verification
+            </p>
+          </motion.div>
+
+          <motion.ul
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 1.4 }}
+            className="font-medium font-neueMontreal  text-xm lg:text-xl"
+          >
+            <li className="flex mb-2  text-justify">
+              <FaRegHandPointRight size={19} className="mt-1" />
+              <span className="pl-2">
+              Seamless Access Control Solutions.
+              </span>
+            </li>
+            <li className="flex mb-2 text-md text-justify">
+              <FaRegHandPointRight size={19} className="mt-1" />
+              <span className="pl-2">
+              Enhanced Security and Surveillance.
+              </span>
+            </li>
+            <li className="flex mb-2 text-md text-justify">
+              <FaRegHandPointRight size={19} className="mt-1" />
+              <span className="pl-2">
+              Personalized User Experiences.
+              </span>
+            </li>
+            <li className="flex mb-2 text-md text-justify">
+              <FaRegHandPointRight size={19} className="mt-1" />
+              <span className="pl-2">
+              Real-Time Face Detection Technology.
+              </span>
+            </li>
+          </motion.ul>
+        </div>
+
+        {/* Image */}
+        <motion.div
+          className="mt-2 col-span-1"
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={isVisible ? { opacity: 1, scale: 1 } : {}}
+          transition={{ duration: 0.8, delay: 1.4 }}
+        >
+          <img className="p-2 w-[500px] h-[500px] ml-3" src={right} alt="testimonial" />
+        </motion.div>
+      </div>
+      <div
+        ref={ref}
+        className=" mt-16  p-2 max-w-[1240px] mx-auto my-3 md:grid grid-cols-2"
+      >
+        {/* Text Content */}
+        {/* Image */}
+        <motion.div
+          className="mt-2 col-span-1"
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={isVisible ? { opacity: 1, scale: 1 } : {}}
+          transition={{ duration: 0.8, delay: 1.4 }}
+        >
+          <img className="p-2 w-[500px] h-[700px]" src={left} alt="testimonial" />
+        </motion.div>
+        <div className=" ml-7 font-playfair col-span-1 p-3  flex flex-col justify-center">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 0.5 }}
+          ></motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 0.8 }}
+          >
+            <h1 className=" text-4xl lg:text-5xl font-foundersGrotesk font-bold  text-dark-blue mb-4">
+              ABOUT US
+            </h1>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={isVisible ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 0.8, delay: 1.1 }}
+          >
+            <p className="text-xm lg:text-xl font-neueMontreal font-medium text-gray-600 mb-5">
+              At <span className=" mx-1 text-3xl text-white font-foundersGrotesk ">
+                {" "}
+                CYLSETECH
+              </span>{" "}
+              , we’re pioneering the future with AI-driven solutions that transform industries. From intelligent video analytics to cutting-edge facial recognition and dynamic crowd management, we use technology to solve real-world challenges
+We’re not just building products; we’re creating smarter, safer environments where businesses thrive and people feel secure. With a focus on innovation and real-time impact, we’re empowering organizations to unlock new possibilities, streamline operations, and make data-driven decisions that shape a better tomorrow.
+            </p>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
